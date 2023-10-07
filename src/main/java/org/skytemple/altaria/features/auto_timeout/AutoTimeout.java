117e814680690ea552467a8ca6b2c690aeb3450b
End45 <71291438+End45@users.noreply.github.com>
2023-10-07 13:18:19 +0200

    Refactor parseVortexTime into a more generic class

--- src/main/java/org/skytemple/altaria/features/auto_timeout/AutoTimeout.java
+++ src/main/java/org/skytemple/altaria/features/auto_timeout/AutoTimeout.java
@@ -26,22 +26,18 @@ import org.skytemple.altaria.definitions.db.AutoTimeoutDB;
 import org.skytemple.altaria.definitions.db.Database;
 import org.skytemple.altaria.definitions.exceptions.DbOperationException;
 import org.skytemple.altaria.definitions.senders.ChannelMsgSender;
-import org.skytemple.altaria.definitions.senders.MessageSender;
 import org.skytemple.altaria.definitions.singletons.ApiGetter;
 import org.skytemple.altaria.definitions.singletons.ExtConfig;
+import org.skytemple.altaria.utils.DurationParser;
 import org.skytemple.altaria.utils.Utils;
 
 import java.time.Duration;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Used to automatically timeout members when they receive a Vortex strike, since Vortex cannot do it on its own.
  */
 public class AutoTimeout {
 	private static final long VORTEX_ID = 240254129333731328L;
-	// Vortex supports more complex time strings, but this should be enough.
-	private static final Pattern VORTEX_TIME_FORMAT = Pattern.compile("(\\d+)([a-z]?)");
 
 	private final DiscordApi api;
 	private final ExtConfig extConfig;
@@ -84,14 +80,18 @@ public class AutoTimeout {
 						} else if (splitMsg.length == 4 && (action.equals("mute") || action.equals("tempmute")
 						|| action.equals("temp-mute"))) {
 							String durationStr = splitMsg[3];
-							int timeSeconds;
+							Duration timeSeconds;
 							try {
-								timeSeconds = parseVortexTime(durationStr, sender);
+								// DurationParser can't parse more complex time strings (which Vortex does support),
+								// but this should be enough.
+								timeSeconds = DurationParser.parse(durationStr);
 							} catch (IllegalArgumentException e) {
-								// Ignore
+								sender.send("Error: Invalid time string. If Vortex accepted the command, try again " +
+									"with a simpler time format string (number + single letter) to ensure I accept " +
+									"it too.");
 								return;
 							}
-							db.setDuration(numStrikes, timeSeconds);
+							db.setDuration(numStrikes, (int) timeSeconds.toSeconds());
 							logger.info("Updated auto-mute punishment for " + numStrikes + " strikes: " + timeSeconds +
 								" seconds.");
 						}
@@ -127,43 +127,6 @@ public class AutoTimeout {
 					}
 				}
 			}
-		}
-	}
-
-	/**
-	 * Given a string that represents a Vortex punishment duration, returns the equivalent in seconds.
-	 * @param timeStr String that represents the duration
-	 * @param errorSender Used to send error messages
-	 * @throws IllegalArgumentException If the time string provided is invalid
-	 * @return Duration in seconds
-	 */
-	private int parseVortexTime(String timeStr, MessageSender errorSender) {
-		Matcher matcher = VORTEX_TIME_FORMAT.matcher(timeStr);
-		if (matcher.matches()) {
-			String num = matcher.group(1);
-			String unit = matcher.group(2);
-			int numInt;
-			try {
-				numInt = Integer.parseInt(num);
-			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException(e);
-			}
-			switch (unit) {
-				case "m":
-					numInt *= 60;
-					break;
-				case "h":
-					numInt *= 60 * 60;
-					break;
-				case "d":
-					numInt *= 60 * 60 * 24;
-					break;
-			}
-			return numInt;
-		} else {
-			errorSender.send("Error: Invalid time string. If Vortex accepted the command, try again with a simpler time " +
-				"format string (number + single letter) to ensure I accept it too.");
-			throw new IllegalArgumentException("Invalid time string");
 		}
 	}
 }
---
+++ src/main/java/org/skytemple/altaria/utils/DurationParser.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2023. End45 and other contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.skytemple.altaria.utils;
+
+import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Used to parse strings that represent a time duration.
+ */
+public class DurationParser {
+	private static final Pattern TIME_FORMAT = Pattern.compile("^(\\d+)([smhd])$");
+
+	/**
+	 * Given a string that represents a time duration, it as a {@link Duration} object.
+	 * The strings to parse are expected to have the format {@code <number><unit>}, with {@code <unit>} being a single
+	 * lowercase letter that represents a time unit and {@code <number>} being the amount.
+	 * @param str String that represents the duration
+	 * @throws IllegalArgumentException If the time string provided does not follow the expected format
+	 * @return Duration representing the specified duration
+	 */
+	public static Duration parse(String str) {
+		Matcher matcher = TIME_FORMAT.matcher(str);
+		if (matcher.matches()) {
+			String num = matcher.group(1);
+			String unit = matcher.group(2);
+			int numInt;
+			try {
+				numInt = Integer.parseInt(num);
+			} catch (NumberFormatException e) {
+				throw new IllegalArgumentException(e);
+			}
+			numInt *= getTimeUnitMultiplier(unit);
+			return Duration.ofSeconds(numInt);
+		} else {
+			throw new IllegalArgumentException("Invalid time string");
+		}
+	}
+
+	/**
+	 * Given a string that contains a single character representing a time unit (or an empty string if no time unit was
+	 * provided), returns its equivalent in seconds.
+	 * @param unit String containing the time unit
+	 * @return Number of seconds represented by a single unit of the specified type
+	 */
+	private static int getTimeUnitMultiplier(String unit) {
+		return switch (unit) {
+			case "s" -> 1;
+			case "m" -> 60;
+			case "h" -> 60 * 60;
+			case "d" -> 60 * 60 * 24;
+			default -> throw new IllegalArgumentException("Invalid time unit");
+		};
+	}
+}
