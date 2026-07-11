9d6af6793e78db3d610ce72b862802a041cb0339
Your Name <you@example.com>
2026-07-11 10:37:49 -0700

    get your own id

--- src/main/java/org/skytemple/altaria/features/reputation/Reputation.java
+++ src/main/java/org/skytemple/altaria/features/reputation/Reputation.java
@@ -263,7 +263,7 @@ public class Reputation {
 			event.getMessageAuthor().getId() == SPRITEBOT_ID) {
 			String[] message = event.getMessage().getContent().split(" ");
 			// check if the first argument is a ping to the bot
-			if (!message[0].equals("<@" + SPRITEBOT_ID + ">")) {
+			if (!message[0].equals("<@" + api.getYourself().getId() + ">")) {
 				// Remove the first element, which is the bot's mention
 				message = Arrays.copyOfRange(message, 1, message.length);
 			}
